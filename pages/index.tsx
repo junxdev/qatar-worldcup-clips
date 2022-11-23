13dd8708d69f711fcdae2ef70e2562bb41ad4665
Jun Park <junxdev@gmail.com>
2022-11-24 02:56:14 +0900

    add description about no spoiler

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,6 +58,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>2022 카타르 월드컵 하이라이트 모음</h1>
+        <p className={styles.description}>노스포일러를 위해 KBS 영상만 제공됩니다 - 2022. 11. 23.</p>
         <div className={styles.grid}>
           {videos.map((v) => (
             <Link key={v.id} href={`/highlight/${v.id}`}>
