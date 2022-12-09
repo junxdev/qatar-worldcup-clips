9f7a8e2badbfa78842c3eb0d8a221cdfd8d53b80
Jun Park <junxdev@gmail.com>
2022-12-09 11:50:50 +0900

    streaming cors test

--- components/player.tsx
+++ components/player.tsx
@@ -8,15 +8,15 @@ type Props = {
 export default function Player({ url }: Props) {
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  function renderVideo(s: string) {
+  function renderVideo(url: string) {
     if (!videoRef.current) return;
     if (Hls.isSupported()) {
       var hls = new Hls();
 
-      hls.loadSource(s);
+      hls.loadSource(url);
       hls.attachMedia(videoRef.current!);
     } else if (videoRef.current!.canPlayType("application/vnd.apple.mpegurl")) {
-      videoRef.current!.src = s;
+      videoRef.current!.src = url;
     } else {
       console.error(
         "This is an old browser that does not support MSE https://developer.mozilla.org/en-US/docs/Web/API/Media_Source_Extensions_API"
